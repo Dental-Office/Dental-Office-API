cd63a6dece38cdee9463f4f6007b7339989b8318
Dragana Spasojevic <spasojevicdragana1989@gmail.com>
2022-03-21 20:39:37 +0100

    initialize project

---
+++ src/main/java/com/dentaloffice/DentalofficeApplication.java
@@ -0,0 +1,13 @@
+package com.dentaloffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalofficeApplication.class, args);
+	}
+
+}
