39120f3cd937421dca87cb8d7d7f2cb2a52a94f3
Dragana Spasojevic <spasojevicdragana1989@gmail.com>
2022-04-01 19:59:35 +0200

    Created DB for Patient with Flyway

---
+++ src/main/resources/db/migration/V2__Add_patient_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE patient (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    date_of_birth VARCHAR NOT NULL,
+    phone_number VARCHAR NOT NULL
+);
+
+
+
