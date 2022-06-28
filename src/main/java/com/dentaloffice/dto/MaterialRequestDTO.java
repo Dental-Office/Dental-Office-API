40fd185d71f5258ee2aecd8acb34bd0df55126e0
Dragana Spasojevic <spasojevicdragana1989@gmail.com>
2022-06-28 18:07:54 +0200

    fixing bug for validation on quantity field

--- src/main/java/com/dentaloffice/dto/MaterialRequestDTO.java
+++ src/main/java/com/dentaloffice/dto/MaterialRequestDTO.java
@@ -2,6 +2,7 @@ package com.dentaloffice.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -14,7 +15,8 @@ public class MaterialRequestDTO {
     @Pattern(regexp = "^[a-zA-Z]*", message = "First name can not be a number!")
     private String materialName;
 
-    @NotNull(message = "Quantity can not be null!")
-//    @Pattern(regexp = "[0-9] {2,5}", message = "Quantity can not be a character and must be grater than 1 or less than 5 numbers!")
+    @NotBlank(message = "Quantity can not be null!")
+    @Pattern(regexp = "[0-9]{1,5}", message = "Phone number can not be a character and must be grater than 1 or less than 5 numbers!")
+//    @Pattern(regexp = "[0-9]", message = "Quantity can not be a character and must be grater than 1 or less than 5 numbers!")
     private String quantity;
 }
